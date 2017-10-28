6457925f4e14b6e52e8064b5e6004d05dca73606
harmless <harmless@syneforge.com>
2017-10-28 20:16:06 +0300

    Fix db update datetime

--- inc/classes/Nod32ms.class.php
+++ inc/classes/Nod32ms.class.php
@@ -678,10 +678,11 @@ class Nod32ms
 
                                 if (empty($old_version)) {
                                     Log::informer(Language::t("Your database was successfully updated to %s", $mirror['db_version']), Mirror::$version, 2);
+                                    $this->fix_time_stamp();
                                 } elseif (!empty($total_downloads[Mirror::$version])) {
                                     Log::informer(Language::t("Your database was successfully updated from %s to %s", $old_version, $mirror['db_version']), Mirror::$version, 2);
+                                    $this->fix_time_stamp();
                                 }
-                                $this->fix_time_stamp();
                                 break;
                             }
                         }
