a6704573d0fa3866128ce3b8adde48a11b9dfe11
zxzharnlesszxz <zxzharnlesszxz@gmail.com>
2018-03-01 19:21:06 +0200

    Debug reloading

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ require __DIR__ . "/inc/init.php";
 
 if ($try_self_update()) {
     spl_autoload_unregister($autoload);
-    unset($autoload, $try_self_update);
+    unset($autoload, $try_self_update, $CONSTANTS);
     require __DIR__ . "/inc/init.php";
 }
 
