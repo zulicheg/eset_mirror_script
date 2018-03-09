266b4e5ee51f61310327f7c816db201bd0a19312
zxzharnlesszxz <zxzharnlesszxz@gmail.com>
2018-03-09 13:05:29 +0200

    Fix typo

--- inc/classes/Mirror.class.php
+++ inc/classes/Mirror.class.php
@@ -464,6 +464,7 @@ class Mirror
                     if (next(static::$mirrors)) {
                         Log::write_log(Language::t("Try next mirror %s", current(static::$mirrors)['host']), 3, static::$version);
                         var_dump(str_replace(prev(static::$mirrors)['host'], next(static::$mirrors)['host'], $info['url']));
+                        Log::write_log(Language::t("Try next mirror %s", current(static::$mirrors)['host']), 3, static::$version);
                         $options[CURLOPT_URL] = str_replace(prev(static::$mirrors)['host'], next(static::$mirrors)['host'], $info['url']);
                         var_dump($options);
                         curl_multi_remove_handle($master, $ch);
